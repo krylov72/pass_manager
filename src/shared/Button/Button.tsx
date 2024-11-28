245a59899623ce50326e6f5262ed06841cc14699
krylov72 <qobject1337@gmail.com>
2024-11-28 16:26:02 +0500

    pass_manager feat: create custom Button component

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,11 +1,16 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from 'react'
 import s from './Button.module.scss'
+import clsx from 'clsx'
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement>
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & { className?: string }
 
-export const Button = ({ children, ...rest }: PropsWithChildren<Props>) => {
+export const Button = ({
+  children,
+  className,
+  ...rest
+}: PropsWithChildren<Props>) => {
   return (
-    <button className={s.button} {...rest}>
+    <button className={clsx(s.button, className)} {...rest}>
       {children}
     </button>
   )
