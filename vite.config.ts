87e447614b0f2ca8785546d7190331a2c56f67f3
krylov72 <qobject1337@gmail.com>
2024-11-28 16:23:30 +0500

    pass_manager fix: add dependency, fix settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@shared': resolve(__dirname, './src/shared'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@components': resolve(__dirname, './src/components'),
+      '@styles': resolve(__dirname, './src/styles'),
+      '@utils': resolve(__dirname, './src/utils'),
+    },
+  },
 })
