13bfde6e7b08f9ed0526d873fdb3e521503a9706
krylov72 <qobject1337@gmail.com>
2024-11-28 16:26:32 +0500

    pass_manager feat: create Context for app

---
+++ src/shared/LocalStorageContext/LocalStorageContext.tsx
@@ -0,0 +1,48 @@
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+import { FormCommonType, LocalStorageContextType } from 'src/types'
+
+const LocalStorageContext = createContext<LocalStorageContextType | undefined>(
+  undefined
+)
+
+//Решение приходило очень долго, потому что логика реализована таким образом:
+//Пароль сетается в другой компоненте, а передавать пропсы из низа наверх и потом опять вниз не лучший выход, лишний код
+//Если будет необходимо, реализую через пропсы
+
+export const useLocalStorage = () => {
+  return useContext(LocalStorageContext)
+}
+
+export const LocalStorageProvider = ({ children }: PropsWithChildren) => {
+  const [items, setItems] = useState<Array<[string, string]>>([])
+
+  const getItemsFromLocalStorage = () => {
+    return Object.entries({ ...localStorage })
+  }
+
+  const addItem = (data: FormCommonType) => {
+    localStorage.setItem(data.service, data.password)
+    setItems(getItemsFromLocalStorage())
+  }
+
+  const removeItem = (key: string) => {
+    localStorage.removeItem(key)
+    setItems(getItemsFromLocalStorage())
+  }
+
+  useEffect(() => {
+    setItems(getItemsFromLocalStorage())
+  }, [])
+
+  return (
+    <LocalStorageContext.Provider value={{ items, addItem, removeItem }}>
+      {children}
+    </LocalStorageContext.Provider>
+  )
+}
