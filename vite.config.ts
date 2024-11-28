50a46137d048ba58871507d52cfa5fb6e7dd4764
krylov72 <qobject1337@gmail.com>
2024-11-28 16:49:53 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       '@shared': resolve(__dirname, './src/shared'),
