f4f3cbf9d57dd1100738ab5f475840173d49c7b7
krylov72 <qobject1337@gmail.com>
2024-11-28 16:25:55 +0500

    pass_manager feat: create custom Alert component

--- src/shared/Alert/Alert.tsx
+++ src/shared/Alert/Alert.tsx
@@ -1,18 +1,33 @@
-import { createPortal } from "react-dom";
-import Button from "../Button/Button";
-import s from "./Alert.module.scss";
+import { createPortal } from 'react-dom'
+import Button from '../Button/Button'
+import s from './Alert.module.scss'
+import { useEffect, useRef } from 'react'
 
 type Props = {
-  message: string;
-  onClose: () => void;
-};
+  message: string
+  onClose: () => void
+  timeOnClose: number
+  success: boolean
+}
 
-export const Alert = ({ message, onClose }: Props) => {
+export const Alert = ({ message, onClose, timeOnClose, success }: Props) => {
+  const timerId = useRef<ReturnType<typeof setTimeout> | null>(null) //берем тип у setTimeout func || null
+  useEffect(() => {
+    timerId.current = setTimeout(onClose, timeOnClose)
+
+    return () => {
+      if (timerId.current) {
+        clearTimeout(timerId.current)
+      } // Убиваем таймер при размонтировании компонента
+    }
+  }, [onClose, timeOnClose])
   return createPortal(
-    <div className={s.container}>
+    <div className={`${s.container} ${success ? s.success : s.error}`}>
       <p>{message}</p>
-      <Button onClick={onClose}>X</Button>
+      <Button className={s.alert__button} onClick={onClose}>
+        Close
+      </Button>
     </div>,
     document.body
-  );
-};
+  )
+}
