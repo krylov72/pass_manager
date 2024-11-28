098bb0f6d2d96e9b518c824ebfbfa1a0d15ec030
krylov72 <qobject1337@gmail.com>
2024-11-28 09:50:56 +0500

    pass_manager fix: add components to main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import './App.css'
+import { Container } from './components/Container/Container'
+import { Menu } from './components/Menu/Menu'
+import './App.scss'
+import { AddPasswordModal } from './components/AddPasswordModal/AddPasswordModal'
+import { useState } from 'react'
+import { PasswordContainer } from './components/PasswordContainer/PasswordContainer'
 
 function App() {
-  return <></>
+  const [isAddPasswordOpen, setIsAddPasswordOpen] = useState(false)
+  const [isContainerOpen, setIsContainerOpen] = useState(false)
+
+  return (
+    <div className='page'>
+      <Container>
+        {isAddPasswordOpen && <AddPasswordModal />}
+        {isContainerOpen && <PasswordContainer />}
+        <Menu
+          isAddPaswordOpen={() => setIsAddPasswordOpen(!isAddPasswordOpen)}
+          isContainerOpen={() => setIsContainerOpen(!isContainerOpen)}
+        />
+      </Container>
+    </div>
+  )
 }
 
 export default App
